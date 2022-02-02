a0906cce46a166805c1647cb49ba43d968183160
Wo-oJin <dnwls813@ajou.ac.kr>
2022-02-02 23:39:54 +0900

    Member management web application using jsp
    ** functions provided **
    1. Add a new Member
    2. View all Member

---
+++ jsp_study/src/main/java/woojin/jsp_study/JspStudyApplication.java
@@ -0,0 +1,13 @@
+package woojin.jsp_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspStudyApplication.class, args);
+	}
+
+}
---
+++ jsp_study/src/test/java/woojin/jsp_study/JspStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package woojin.jsp_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
