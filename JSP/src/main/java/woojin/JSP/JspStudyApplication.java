4cdab2197b98912dbe0b353f399f8dcf3f41694b
Wo-oJin <dnwls813@ajou.ac.kr>
2022-02-08 00:33:46 +0900

    Create view through HTML generation using JSP

---
+++ JSP/src/main/java/woojin/JSP/JspStudyApplication.java
@@ -0,0 +1,13 @@
+package woojin.JSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspStudyApplication.class, args);
+	}
+
+}
---
+++ JSP/src/test/java/woojin/JSP/JspStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package woojin.JSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
