bd1301443dd48a22e9139fca4e8623a6f7cc0999
Wo-oJin <dnwls813@ajou.ac.kr>
2022-02-08 23:12:37 +0900

    Add ControllerV4HandlerAdapter

--- Front-Controller/src/main/java/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ Front-Controller/src/main/java/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -17,7 +17,11 @@ import servlet.web.frontcontroller.MyView;
 import servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
 import servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
 import servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
+import servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 
 @WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
 public class FrontControllerServletV5 extends HttpServlet{
@@ -34,10 +38,15 @@ public class FrontControllerServletV5 extends HttpServlet{
 		handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
 		handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
 		handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+		
+		handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+		handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+		handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
 	}
 	
 	private void initHandlerAdapters() {
 		handlerAdapters.add(new ControllerV3HandlerAdapter());
+		handlerAdapters.add(new ControllerV4HandlerAdapter());
 	}
 
 	@Override
